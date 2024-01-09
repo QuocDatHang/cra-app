20a4d75f40a4ee0f3a7ae0de35260bc76cfdfc8e
unknown <quocdathang1997@gmail.com>
2024-01-09 21:00:34 +0700

    init MUI

--- src/index.js
+++ src/index.js
@@ -5,14 +5,11 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import { BrowserRouter } from 'react-router-dom';
 import ThemeProvider from './components/Context/ThemeContext';
 import { ToastContainer } from 'react-toastify';
-<<<<<<< HEAD
 import BankingApp from './components/BankingApp/BankingApp';
 import CreateUser from './components/User/CreateUser';
 import App from './App';
 import Employee from './components/Employee/Employee';
 import EmployeeManagement from './components/Employee/EmployeeManagement';
-=======
->>>>>>> f7524968113dae083c4eb82070ffb5ecf2af9855
 import CkEditor from './components/CkEditor/CkEditor';
 import UploadImage from './components/UploadImage/UploadImage';
 
